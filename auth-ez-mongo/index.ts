e08a37850a3b60ac7aa25a8e578f882f6b24c71c
Usaid <46266811+usaidpeerzada@users.noreply.github.com>
2023-12-15 02:35:32 +0530

    Update index.ts

--- auth-ez-mongo/index.ts
+++ auth-ez-mongo/index.ts
@@ -2,19 +2,30 @@ import express from "express";
 import mongoose from "mongoose";
 import { User } from "./user.model";
 import { Config } from "./types";
-import { Resend } from "resend";
-import fetch, { Headers } from "node-fetch";
+// import { Resend } from "resend";
+// import { CreateMongoAuthController } from "auth-ez-local";
 import { CreateMongoAuthController } from "auth-ez";
 import dotenv from "dotenv";
+import nodemailer from "nodemailer";
 
 dotenv.config();
 
 const app = express();
-const resend = new Resend(process.env.RESEND_API_KEY);
+// const resend = new Resend(process.env.RESEND_API_KEY);
 const port = 3000;
 const mongoUrl = process.env.MONGO_URL || "mongodb://localhost:27017/test";
 
 mongoose.connect(mongoUrl);
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  host: "'smtp.gmail.com'",
+  port: 587,
+  secure: false,
+  auth: {
+    user: "",
+    pass: "",
+  },
+});
 const config: Config = {
   // Send user model to the auth-ez controller:
   User: User,
@@ -39,21 +50,21 @@ const config: Config = {
   // email options for sending verification email and reset password link - this is optional but if enabled all other params inside are required.
   emailOptions: {
     enableEmail: true,
-    emailType: "resend",
-    emailSdk: resend,
-    forgotPasswordSubject: "",
-    forgotPasswordBody: "",
-    verificationMailSubject: "Sending custom subejct from config",
-    verificationMailBody: `here is the body bro ${mongoUrl}`,
-    // emailService: new EmailService(this),
+    emailType: "nodemailer",
+    emailSdk: transporter,
+    // forgotPasswordSubject: "",
+    // forgotPasswordBody: "",
+    // verificationMailSubject: "Sending custom subejct from config",
+    // verificationMailBody: `here is the body bro ${mongoUrl}`,
+    // emailService: myMailService,
   },
 };
 
 const authController = new CreateMongoAuthController(config);
 app.use("/auth", authController.getRouter());
 app.get("/auth/protected-route", (req, res) => {
-  console.log("HEADERS -> ", req.headers);
-  res.send("Protected route is working");
+  console.log(req);
+  res.json({ msg: "Protected route is working" });
 });
 
 app.listen(port, () => {
